2dcd71fc0ab330d784a9673d26e879add75eb0d6
Ori Lazar <36171247+Kney-Delach@users.noreply.github.com>
2019-12-08 10:19:24 +0000

    updated comments

--- Game-Technologies-CSC8503/CSC8503/CSC8503Common/QuadTree.cpp
+++ Game-Technologies-CSC8503/CSC8503/CSC8503Common/QuadTree.cpp
@@ -1,3 +1,21 @@
+/***************************************************************************
+* Filename		: QuadTree.cpp
+* Name			: Ori Lazar
+* Date			: 31/10/2019
+* Description	: Represents a quad tree spatial acceleration structure.
+    .---.
+  .'_:___".
+  |__ --==|
+  [  ]  :[|
+  |__| I=[|
+  / / ____|
+ |-/.____.'
+/___\ /___\
+***************************************************************************/
 #include "QuadTree.h"
 #include "../../Common/Vector3.h"
-using namespace NCL;
+
+namespace NCL
+{
+	
+}
--- Game-Technologies-CSC8503/CSC8503/CSC8503Common/QuadTree.h
+++ Game-Technologies-CSC8503/CSC8503/CSC8503Common/QuadTree.h
@@ -1,98 +1,127 @@
+/***************************************************************************
+* Filename		: QuadTree.h
+* Name			: Ori Lazar
+* Date			: 31/10/2019
+* Description	: QuadTree        -> Represents a quad tree spatial acceleration structure.
+*                 QuadTreeNode    -> Represents an individual part of the quadtree.
+*                 QuadTreeEntries -> Multiples of these can be added to a tree node (contains position and size).
+    .---.
+  .'_:___".
+  |__ --==|
+  [  ]  :[|
+  |__| I=[|
+  / / ____|
+ |-/.____.'
+/___\ /___\
+***************************************************************************/
 #pragma once
 #include "../../Common/Vector2.h"
 #include "Debug.h"
 #include <list>
 #include <functional>
 
-namespace NCL {
+namespace NCL
+{
 	using namespace NCL::Maths;
-	namespace CSC8503 {
+	
+	namespace CSC8503
+	{
 		template<class T>
 		class QuadTree;
 
+		// Entries 
 		template<class T>
-		struct QuadTreeEntry {
-			Vector3 pos;
+		struct QuadTreeEntry
+		{
+			Vector3 position;
 			Vector3 size;
 			T object;
-
-			QuadTreeEntry(T obj, Vector3 pos, Vector3 size) {
-				object		= obj;
-				this->pos	= pos;
-				this->size	= size;
-			}
+			
+			QuadTreeEntry(T obj, Vector3 pos, Vector3 s) : position(pos), size(s), object(obj) {}
 		};
 
+		// Nodes
 		template<class T>
-		class QuadTreeNode	{
+		class QuadTreeNode
+		{
 		public:
-			typedef std::function<void(std::list<QuadTreeEntry<T>>&)> QuadTreeFunc;
+			typedef std::function<void(std::list<QuadTreeEntry<T>>&)> QuadTreeFunction;
 		protected:
 			friend class QuadTree<T>;
 
-			QuadTreeNode() {}
+			QuadTreeNode() = default;
 
-			QuadTreeNode(Vector2 pos, Vector2 size) {
+			QuadTreeNode(Vector2 pos, Vector2 size)
+			{
 				children		= nullptr;
 				this->position	= pos;
 				this->size		= size;
 			}
 
-			~QuadTreeNode() {
+			~QuadTreeNode()
+			{
 				delete[] children;
 			}
 
-			void Insert(T& object, const Vector3& objectPos, const Vector3& objectSize, int depthLeft, int maxSize) {
+			void Insert(T& object, const Vector3& objectPos, const Vector3& objectSize, int depthLeft, int maxSize)
+			{
 			}
 
-			void Split() {
+			void Split()
+			{
 			}
 
-			void DebugDraw() {
+			void DebugDraw()
+			{
 			}
 
-			void OperateOnContents(QuadTreeFunc& func) {
+			void OperateOnContents(QuadTreeFunction& func)
+			{
 			}
 
 		protected:
-			std::list< QuadTreeEntry<T> >	contents;
-
+			std::list<QuadTreeEntry<T>> contents;
 			Vector2 position;
 			Vector2 size;
-
 			QuadTreeNode<T>* children;
 		};
 	}
 }
 
 
-namespace NCL {
+namespace NCL
+{
 	using namespace NCL::Maths;
-	namespace CSC8503 {
+	
+	namespace CSC8503
+	{
+		// Tree
 		template<class T>
 		class QuadTree
 		{
 		public:
-			QuadTree(Vector2 size, int maxDepth = 6, int maxSize = 5){
+			QuadTree(Vector2 size, int maxDepth = 6, int maxSize = 5)
+			{
 				root = QuadTreeNode<T>(Vector2(), size);
 				this->maxDepth	= maxDepth;
 				this->maxSize	= maxSize;
 			}
-			~QuadTree() {
-			}
+			~QuadTree() = default;
 
-			void Insert(T object, const Vector3& pos, const Vector3& size) {
+			void Insert(T object, const Vector3& pos, const Vector3& size)
+			{
 				root.Insert(object, pos, size, maxDepth, maxSize);
 			}
 
-			void DebugDraw() {
+			void DebugDraw()
+			{
 				root.DebugDraw();
 			}
 
-			void OperateOnContents(typename QuadTreeNode<T>::QuadTreeFunc  func) {
+			void OperateOnContents(typename QuadTreeNode<T>::QuadTreeFunction  func)
+			{
 				root.OperateOnContents(func);
 			}
-
 		protected:
 			QuadTreeNode<T> root;
 			int maxDepth;
